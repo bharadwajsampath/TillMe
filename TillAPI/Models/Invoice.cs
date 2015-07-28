5f4c48a5c0f9be94a8219c2d63dbff5d69306e73
bajju <bharadwajsampath@live.com>
2015-07-29 01:46:49 +1000

    newcheck

---
+++ TillAPI/Models/Invoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TillAPI.Models
+{
+   public  class Invoice
+    {
+       public int Id { get; set; }
+       public double Amount { get;set;}
+
+       public int ChequeNumber { get; set; }
+
+       public DateTime Date { get; set; }
+       public double GST { get; set; }
+
+       public string InvoiceNumber { get; set; }
+
+       public string Paid { get; set; }
+
+       public string PaymentType { get; set; }
+
+       public string SupplierName { get; set; }
+       public DateTime TimeCreated { get; set; }
+
+       public virtual Employee FromUser { get; set; }
+    }
+}
---
+++ TillMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TillMe.Startup))]
+namespace TillMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
